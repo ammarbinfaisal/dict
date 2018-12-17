d131d291f89a18f88dd994621997a98c78aadd80
Ammar <30975614+ammarbinfaisal@users.noreply.github.com>
2018-12-18 01:34:56 +0530

    now quite better

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import FSG from "./components/FSG";
+import WordInput from "./components/WordInput";
 import "./App.css";
 
 class App extends Component {
@@ -8,7 +9,9 @@ class App extends Component {
     this.state = {};
   }
   handleWordInput(event) {
-    event.preventDefault();
+    if (event)
+      event.preventDefault();
+    this.setState({ loading: true, reqFailed: false, data: undefined });
     let word = document.querySelector("#word-input").value;
     fetch(
       `https://googledictionaryapi.eu-gb.mybluemix.net/?lang=en&define=${word}`
@@ -16,28 +19,27 @@ class App extends Component {
       .then(res => res.json())
       .then(dataReceived => {
         console.log(dataReceived);
-        this.setState({ data: dataReceived[0] });
-      });
+        this.setState({ data: dataReceived[0], loading: false, reqFailed: false });
+      })
+      .catch(err => {
+        console.log("Request failed perhasp due to wrong spelling");
+        this.setState({ reqFailed: true, loading: false, data: undefined });
+      })
     // .then(res => this.setState({ wordData: res[0] }));
   }
   render() {
     let meanings = [], i = 0;
     if (this.state.data)
       for (let figureOfSpeech in this.state.data.meaning) {
-        meanings[i] = <FSG name={figureOfSpeech} data={this.state.data.meaning[figureOfSpeech]} />;
+        meanings[i] = <FSG name={figureOfSpeech} data={this.state.data.meaning[figureOfSpeech]} key={i} define={this.handleWordInput.bind(this)} />;
         i++;
       }
     return (
-      <div className="App">
-        <div id="input" className="flexrow">
-          <input id="word-input" placeholder="Enter a word" />
-          <button type="button" onClick={this.handleWordInput.bind(this)}>
-            Submit
-          </button>
-        </div>
+      <div className="columns">
+        <WordInput onInput={this.handleWordInput.bind(this)} />
         {/* prettier-ignore */}
         {
-          this.state.data !== undefined ?
+          this.state.data !== undefined && !this.state.reqFailed ?
             (
               <section id="word-definition">
                 <h2>{this.state.data.word}</h2>
@@ -45,7 +47,14 @@ class App extends Component {
                 {meanings}
               </section>
             ) :
-            ""
+            <></>
+        }
+        {
+          this.state.loading ? <p id="loading" className="monospace">loading...</p> : <></>
+        }
+        {
+          this.state.reqFailed ? <p className="monospace error">⚠️ Sorry couldn&apos;t fetch definition of the word.<br /> Either the spelling is wrong or you aren&apos;
+t connected to internet</p> : <></>
         }
       </div>
     );
